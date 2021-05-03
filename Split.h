5a8c9308e1de1592aebaa4418877a6299e7f43f9
Basil3k <55461619+Basil3k@users.noreply.github.com>
2021-05-03 21:18:22 +0300

    adding comments

--- Split.h
+++ Split.h
@@ -2,5 +2,5 @@
 #include <vector>
 #include <string>
 #include <sstream>
-
+// header containing in common folder but .cpp file in server folder. move header to server folder
 std::vector<std::string> split(const std::string& string, char delimiter);
